ba3812b80b9da965538998e1b762faa2a854944d
sea-grass <dev.seagrass@gmail.com>
2021-12-10 01:42:13 -0500

    Replace html-minifier with rehype and strip script tags from output

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,16 +1,50 @@
-import { minify } from 'html-minifier';
+import { rehype } from 'rehype';
+import rehypePresetMinify from 'rehype-preset-minify';
+import rehypeSanitize from 'rehype-sanitize';
+
+async function minify(html: string) {
+	return (
+		rehype()
+			// Effectively strip script tags from the output
+			// This method has the downside that an element that I use in the future will need to be added to `tagNames` to be preserved.
+			.use(rehypeSanitize, {
+				allowDoctypes: true,
+				tagNames: [
+					'html',
+					'head',
+					'body',
+					'meta',
+					'link',
+					'title',
+					'style',
+					'ul',
+					'li',
+					'a',
+					'img',
+					'div',
+					'span'
+				],
+				strip: ['script'],
+				attributes: {
+					meta: ['charset', 'name', 'content'],
+					html: ['lang'],
+					a: ['href'],
+					img: ['alt', 'src'],
+					'*': ['className', 'id'],
+					link: ['rel', 'href', 'type']
+				}
+			})
+			.use(rehypePresetMinify)
+			.process(html)
+			.then(String)
+	);
+}
 
 export async function handle({ request, resolve }) {
 	const response = await resolve(request);
 
 	if (response.headers['content-type'] === 'text/html') {
-		const body = minify(response.body, {
-			collapseWhitespace: true,
-			collapseBooleanAttributes: true,
-			removeRedundantAttributes: true,
-			useShortDoctype: true,
-			removeComments: true
-		});
+		const body = await minify(response.body);
 		return {
 			...response,
 			body
